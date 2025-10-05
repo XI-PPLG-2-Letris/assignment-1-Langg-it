81e6da09ed62e59a9f834a6f9bf977c2469ef967
Langg-it <ltzy7762@gmail.com>
2025-10-06 01:50:14 +0700

    Add files via upload

---
+++ 1-mini-calculator.js
@@ -0,0 +1,47 @@
+const Tambah = (P, Q) => {
+     P = 30
+     Q = 3
+    console.log (P + Q)
+}
+
+Tambah ()
+
+const Kurang = (P, Q) => {
+     P = 30
+     Q = 3
+    console.log (P - Q)
+}
+
+Kurang ()
+
+const Bagi = (P, Q) => {
+    P = 30
+    Q = 3
+    console.log(P / Q)
+}
+
+Bagi ()
+
+const Kali = (P, Q) => {
+    P = 30
+    Q = 3
+    console.log(P * Q)
+}
+
+Kali ()
+
+const Desimal = (P, Q) => {
+    P = 30
+    Q = 3
+    console.log(P % Q)
+}
+
+Desimal ()
+
+const Pangkat = (P, Q) => {
+    P = 30
+    Q = 3
+    console.log(P ** Q)
+}
+
+Pangkat ()
